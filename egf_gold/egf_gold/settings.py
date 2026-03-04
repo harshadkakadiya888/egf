8a7532464ec24d74db0da1c622fbba5125bec1ac
harshadkakadiya888 <harshadkakadiya888@gmail.com>
2026-03-04 14:56:35 +0530

    allow

--- egf_gold/egf_gold/settings.py
+++ egf_gold/egf_gold/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4v_qq@l)er8knwgb0s_c0=z#1v*k9ev--1r@*k=3+13a^dpahs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
